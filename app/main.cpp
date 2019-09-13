169a3f10da005b4949dedafc9660997fc1e06bb5
Maciej Ziaja <maciejzjg@gmail.com>
2019-09-13 15:18:27 +0200

    Add spdlog external lib example

--- app/main.cpp
+++ app/main.cpp
@@ -1,11 +1,17 @@
 #include "calculator/calculator.hpp"
 #include "calculator/square_root.hpp"
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
 #include <iostream>
 
 int main() {
 	calc::Square_root sqrt;
 	calc::Calculator clc{sqrt};
 	std::cout << clc.add(1, 2) << "\n";
+
+	auto console = spdlog::stdout_color_mt("console");    
+    spdlog::get("console")->info("Hello spdlog!");
+
 	return 0;
 }
 
